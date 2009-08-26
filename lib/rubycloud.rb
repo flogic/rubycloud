b7d6c9cea69d7decd2e4669ba4b66ae8f5f01272
Kevin R. Barnes <vinbarnes@gmail.com>
2009-08-26 16:00:31 -0500

    Instantiate RubyCloud provider
    Attempt to instantiate any given provider name.  Try to match given provider name with available providers.  Raise InvalidProviderError if we can't find one.
    
    Closes http://www.pivotaltracker.com/story/show/1161846

--- lib/rubycloud.rb
+++ lib/rubycloud.rb
@@ -1,9 +1,20 @@
+class InvalidProviderError < ArgumentError; end
+
 class RubyCloud
+  attr_reader :provider
+  
   def initialize(options)
     raise ArgumentError unless options[:provider] and options[:credentials]
-    # RubyCloud::EC2.new(options[:credentials])
+    @provider = modularize_provider_string(options[:provider]).new(options[:credentials])
+  end
+private
+  def modularize_provider_string(str)
+    RubyCloud.const_get(RubyCloud.constants.find{|c| c.upcase == str.gsub('_', '').upcase})
+  rescue TypeError
+    raise InvalidProviderError
   end
 end
 
 Dir['vendor/*'].each { |dir|  $:.unshift(File.join(dir, 'lib')) }
 require 'rubycloud/linode'
+require 'rubycloud/ec2'
--- spec/rubycloud_spec.rb
+++ spec/rubycloud_spec.rb
@@ -1,6 +1,10 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe RubyCloud, '.new' do
+  before do
+    RubyCloud::EC2.stub!(:new)
+  end
+  
   it 'should accept :provider and :credentials' do
     lambda { RubyCloud.new(:provider => 'ec2', 
                            :credentials => {}) }.should.not.raise(ArgumentError)
@@ -14,14 +18,30 @@ describe RubyCloud, '.new' do
     lambda { RubyCloud.new(:provider => 'ec2') }.should.raise(ArgumentError)
   end
 
-  # it 'should instantiate the requested provider' do
-  #   @ec2_provider = mock('EC2 Provider')
-  #   @ec2_provider.should.receive(:new).with({})
-  # 
-  #   RubyCloud.new(:provider => 'ec2', :credentials => {})
-  # end
+  it 'should instantiate the requested provider' do
+    RubyCloud::EC2.should.receive(:new).with({})  
+    RubyCloud.new(:provider => 'ec2', :credentials => {})
+  end
 
-  # it 'should return an instance of the requested provider' do
-  # end
+  it 'should have a provider attribute' do
+    RubyCloud.new(:provider => 'ec2', :credentials => {}).should.respond_to(:provider)
+  end
+  
+  it 'should set the provider attribute' do
+    ec2_instance = mock('EC2 instance')
+    RubyCloud::EC2.stub!(:new).and_return(ec2_instance)
+    RubyCloud.new(:provider => 'ec2', :credentials => {}).provider.should == ec2_instance
+  end
 
+  it 'should try to instantiate any given provider' do
+    RubyCloud.const_set('PwnedFauxProvider', 'xxx')
+    RubyCloud::PwnedFauxProvider.should.receive(:new)
+    RubyCloud.new(:provider => 'pwned_faux_provider', :credentials => {})
+    RubyCloud.send(:remove_const, "PwnedFauxProvider")
+  end
+
+  it 'should complain if provider does not exist' do
+    lambda { RubyCloud.new(:provider => 'pwned_faux_provider', :credentials => {}) }.should.raise InvalidProviderError
+  end
+  
 end
