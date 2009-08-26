a23e78e21e1d6c45875aa31f4e1582192f7792ec
Kevin R. Barnes <vinbarnes@gmail.com>
2009-08-26 14:18:03 -0500

    Added basic RubyCloud initializer
    References http://www.pivotaltracker.com/story/show/1161846

--- lib/rubycloud.rb
+++ lib/rubycloud.rb
@@ -1,4 +1,8 @@
 class RubyCloud
+  def initialize(options)
+    raise ArgumentError unless options[:provider] and options[:credentials]
+    # RubyCloud::EC2.new(options[:credentials])
+  end
 end
 
 require 'rubycloud/linode'
--- spec/rubycloud_spec.rb
+++ spec/rubycloud_spec.rb
@@ -1,5 +1,27 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-describe 'rubycloud' do
-  # specs here
+describe RubyCloud, '.new' do
+  it 'should accept :provider and :credentials' do
+    lambda { RubyCloud.new(:provider => 'ec2', 
+                           :credentials => {}) }.should.not.raise(ArgumentError)
+  end
+
+  it 'should complain if no :provider given' do
+    lambda { RubyCloud.new(:credentials => {}) }.should.raise(ArgumentError)
+  end
+
+  it 'should complain if no :credentials given' do
+    lambda { RubyCloud.new(:provider => 'ec2') }.should.raise(ArgumentError)
+  end
+
+  # it 'should instantiate the requested provider' do
+  #   @ec2_provider = mock('EC2 Provider')
+  #   @ec2_provider.should.receive(:new).with({})
+  # 
+  #   RubyCloud.new(:provider => 'ec2', :credentials => {})
+  # end
+
+  # it 'should return an instance of the requested provider' do
+  # end
+
 end
