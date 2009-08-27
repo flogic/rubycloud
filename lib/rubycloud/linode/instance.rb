fb88947774df634ba0b588fb994052c5e05763d2
Rick Bradley <rick@rickbradley.com>
2009-08-27 14:47:23 -0500

    Delegating unknown linode instances methods to linode node.
    We still throw an exception (with a more helpful error message) when the node itself
    isn't familiar with the method being called.
    
    Also, changing terminology to call the OpenStruct returned by the ruby-linode API
    a "node" instead of an "instance" (which is what we're calling the RubyCloud linode
    wrapper object).
    
    Closes:  http://www.pivotaltracker.com/story/show/1169871

--- lib/rubycloud/linode/instance.rb
+++ lib/rubycloud/linode/instance.rb
@@ -1,28 +1,33 @@
 class RubyCloud::Linode::Instance
-  attr_reader :handle, :instance
+  attr_reader :handle, :node
 
-  def initialize(handle, instance)
+  def initialize(handle, node)
     @handle = handle
-    @instance = instance
+    @node = node
   end
   
   def details(options = {})
-    handle.details({ :instance => instance.linodeid }.merge(options))
+    handle.details({ :instance => node.linodeid }.merge(options))
   end
 
   def start(options = {})
-    handle.start({ :instance => instance.linodeid }.merge(options))
+    handle.start({ :instance => node.linodeid }.merge(options))
   end
 
   def stop(options = {})
-    handle.stop({ :instance => instance.linodeid }.merge(options))
+    handle.stop({ :instance => node.linodeid }.merge(options))
   end
 
   def delete(options = {})
-    handle.delete({ :instance => instance.linodeid }.merge(options))
+    handle.delete({ :instance => node.linodeid }.merge(options))
   end
 
   def restart(options = {})
-    handle.restart({ :instance => instance.linodeid }.merge(options))
+    handle.restart({ :instance => node.linodeid }.merge(options))
+  end
+  
+  def method_missing(meth, *args)
+    raise NoMethodError, %Q{Linode API driver does not provide the method "#{meth}" on instances (more documentation available at:  http://www.linode.com/api/autodoc.cfm?method=linode.list).} unless node.respond_to?(meth)
+    node.send(meth, *args)
   end
 end
--- spec/rubycloud/linode/instance_spec.rb
+++ spec/rubycloud/linode/instance_spec.rb
@@ -1,29 +1,31 @@
 require File.expand_path(File.join(File.dirname(__FILE__), %w[.. .. spec_helper.rb]))
 
+
+
 describe RubyCloud::Linode::Instance, '.new' do
-  it 'should allow a RubyCloud::Linode handle and an instance argument' do
-    lambda { RubyCloud::Linode::Instance.new(:handle, :instance) }.should.not.raise(ArgumentError)
+  it 'should allow a RubyCloud::Linode handle and a Linode API node argument' do
+    lambda { RubyCloud::Linode::Instance.new(:handle, :node) }.should.not.raise(ArgumentError)
   end
   
-  it 'should require a RubyCloud::Linode handle and an instance argument' do
+  it 'should require a RubyCloud::Linode handle and a Linode API node argument' do
     lambda { RubyCloud::Linode::Instance.new(:handle) }.should.raise(ArgumentError)    
   end
   
   it 'should store the RubyCloud::Linode handle' do
-    RubyCloud::Linode::Instance.new(:handle, :instance).handle.should == :handle
+    RubyCloud::Linode::Instance.new(:handle, :node).handle.should == :handle
   end
   
-  it 'should store the instance' do
-    RubyCloud::Linode::Instance.new(:handle, :instance).instance.should == :instance
+  it 'should store the Linode API node' do
+    RubyCloud::Linode::Instance.new(:handle, :node).node.should == :node
   end
 end
 
 describe RubyCloud::Linode::Instance, 'when instantiated' do
   before do
     @handle = Object.new
-    @result_instance = Object.new
-    @result_instance.stub!(:linodeid).and_return(12345)
-    @instance = RubyCloud::Linode::Instance.new(@handle, @result_instance)
+    @node = Object.new
+    @node.stub!(:linodeid).and_return(12345)
+    @instance = RubyCloud::Linode::Instance.new(@handle, @node)
   end
   
   it 'should allow fetching details for this instance' do
@@ -48,19 +50,19 @@ describe RubyCloud::Linode::Instance, 'when instantiated' do
       @instance.details
     end
     
-    it "should pass the instance's id to the handle when calling details" do
-      @handle.should.receive(:details).with(:instance => @result_instance.linodeid)
+    it "should pass the node's id to the handle when calling details" do
+      @handle.should.receive(:details).with(:instance => @node.linodeid)
       @instance.details
     end
 
     it 'should pass along any additional details when calling details on the handle' do
-      @handle.should.receive(:details).with(:instance => @result_instance.linodeid, 'foo' => 'bar')
+      @handle.should.receive(:details).with(:instance => @node.linodeid, 'foo' => 'bar')
       @instance.details('foo' => 'bar')
     end
         
     it "should return the result of calling details on the handle" do
       details_results = Object.new
-      @handle.should.receive(:details).with(:instance => @result_instance.linodeid, 'foo' => 'bar').and_return(details_results)
+      @handle.should.receive(:details).with(:instance => @node.linodeid, 'foo' => 'bar').and_return(details_results)
       @instance.details('foo' => 'bar').should == details_results
     end
   end
@@ -87,19 +89,19 @@ describe RubyCloud::Linode::Instance, 'when instantiated' do
       @instance.start
     end
     
-    it "should pass the instance's id to the handle when calling start" do
-      @handle.should.receive(:start).with(:instance => @result_instance.linodeid)
+    it "should pass the node's id to the handle when calling start" do
+      @handle.should.receive(:start).with(:instance => @node.linodeid)
       @instance.start
     end
 
     it 'should pass along any additional arguments when calling start on the handle' do
-      @handle.should.receive(:start).with(:instance => @result_instance.linodeid, 'foo' => 'bar')
+      @handle.should.receive(:start).with(:instance => @node.linodeid, 'foo' => 'bar')
       @instance.start('foo' => 'bar')
     end
         
     it "should return the result of calling start on the handle" do
       start_results = Object.new
-      @handle.should.receive(:start).with(:instance => @result_instance.linodeid, 'foo' => 'bar').and_return(start_results)
+      @handle.should.receive(:start).with(:instance => @node.linodeid, 'foo' => 'bar').and_return(start_results)
       @instance.start('foo' => 'bar').should == start_results
     end
   end
@@ -126,19 +128,19 @@ describe RubyCloud::Linode::Instance, 'when instantiated' do
       @instance.stop
     end
     
-    it "should pass the instance's id to the handle when calling stop" do
-      @handle.should.receive(:stop).with(:instance => @result_instance.linodeid)
+    it "should pass the node's id to the handle when calling stop" do
+      @handle.should.receive(:stop).with(:instance => @node.linodeid)
       @instance.stop
     end
 
     it 'should pass along any additional arguments when calling stop on the handle' do
-      @handle.should.receive(:stop).with(:instance => @result_instance.linodeid, 'foo' => 'bar')
+      @handle.should.receive(:stop).with(:instance => @node.linodeid, 'foo' => 'bar')
       @instance.stop('foo' => 'bar')
     end
         
     it "should return the result of calling stop on the handle" do
       stop_results = Object.new
-      @handle.should.receive(:stop).with(:instance => @result_instance.linodeid, 'foo' => 'bar').and_return(stop_results)
+      @handle.should.receive(:stop).with(:instance => @node.linodeid, 'foo' => 'bar').and_return(stop_results)
       @instance.stop('foo' => 'bar').should == stop_results
     end
   end
@@ -165,19 +167,19 @@ describe RubyCloud::Linode::Instance, 'when instantiated' do
       @instance.delete
     end
     
-    it "should pass the instance's id to the handle when calling delete" do
-      @handle.should.receive(:delete).with(:instance => @result_instance.linodeid)
+    it "should pass the node's id to the handle when calling delete" do
+      @handle.should.receive(:delete).with(:instance => @node.linodeid)
       @instance.delete
     end
 
     it 'should pass along any additional arguments when calling delete on the handle' do
-      @handle.should.receive(:delete).with(:instance => @result_instance.linodeid, 'foo' => 'bar')
+      @handle.should.receive(:delete).with(:instance => @node.linodeid, 'foo' => 'bar')
       @instance.delete('foo' => 'bar')
     end
         
     it "should return the result of calling delete on the handle" do
       delete_results = Object.new
-      @handle.should.receive(:delete).with(:instance => @result_instance.linodeid, 'foo' => 'bar').and_return(delete_results)
+      @handle.should.receive(:delete).with(:instance => @node.linodeid, 'foo' => 'bar').and_return(delete_results)
       @instance.delete('foo' => 'bar').should == delete_results
     end
   end
@@ -204,20 +206,52 @@ describe RubyCloud::Linode::Instance, 'when instantiated' do
       @instance.restart
     end
     
-    it "should pass the instance's id to the handle when calling restart" do
-      @handle.should.receive(:restart).with(:instance => @result_instance.linodeid)
+    it "should pass the node's id to the handle when calling restart" do
+      @handle.should.receive(:restart).with(:instance => @node.linodeid)
       @instance.restart
     end
 
     it 'should pass along any additional arguments when calling restart on the handle' do
-      @handle.should.receive(:restart).with(:instance => @result_instance.linodeid, 'foo' => 'bar')
+      @handle.should.receive(:restart).with(:instance => @node.linodeid, 'foo' => 'bar')
       @instance.restart('foo' => 'bar')
     end
         
     it "should return the result of calling restart on the handle" do
       restart_results = Object.new
-      @handle.should.receive(:restart).with(:instance => @result_instance.linodeid, 'foo' => 'bar').and_return(restart_results)
+      @handle.should.receive(:restart).with(:instance => @node.linodeid, 'foo' => 'bar').and_return(restart_results)
       @instance.restart('foo' => 'bar').should == restart_results
+    end
+  end
+  
+  describe 'delegating unknown methods' do
+    # helper class to test delegation to Linode nodes
+    class TestNode
+      def supported_method(args = {})
+      end
+    end
+
+    before do
+      @node = TestNode.new
+      @instance = RubyCloud::Linode::Instance.new(@handle, @node)
+    end      
+
+    it "should delegate any unknown methods to the node" do
+      @node.should.receive(:supported_method)
+      @instance.supported_method
+    end
+  
+    it 'should fail when attempting to delegate a method to the node which the node does not support' do
+      lambda { @instance.unknown_method }.should.raise(NoMethodError)
+    end
+  
+    it 'should pass all provided arguments to node when delegating methods' do
+      @node.should.receive(:supported_method).with('foo' => 'bar', 'baz' => 'xyzzy')
+      @instance.supported_method('foo' => 'bar', 'baz' => 'xyzzy')    
+    end
+  
+    it "should return the result of node's computation when delegating methods" do
+      @node.should.receive(:supported_method).with('foo' => 'bar', 'baz' => 'xyzzy').and_return('some results')
+      @instance.supported_method('foo' => 'bar', 'baz' => 'xyzzy').should == 'some results'
     end
   end
 end
